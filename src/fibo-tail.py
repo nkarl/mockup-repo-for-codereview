8c4f7703e3b033fb0adda16e92924613a3500b06
nkarl <21993921+nkarl@users.noreply.github.com>
2022-05-17 13:38:20 -0700

    Setting up the repo for mockup code review: added a source file with fibo sequence code.

--- src/fibo-tail.py
+++ src/fibo-tail.py
@@ -1,33 +1,9 @@
-# tail recursion practice on the Fibonacci sequence
-
-# def fibo(i, prev, curr, buf):
-    # if i == 0:
-        # return buf
-    # else:
-        # return fibo(i-1, curr, prev+curr, prev+curr)
-
-
-# def fib(n):
-    # return fibo(n, 0, 1, 0)
-
-
-def fibo(i, pre, cur):
-    print("stack:{}\t, previous:{}".format(i, pre))
-    if i == 0:
-        return pre
-    return fibo(i-1, cur, pre+cur)
-    # return pre if i == 0 else fibo(i-1, cur, pre+cur)
-
-
 def fib(n):
-    return fibo(n, 0, 1)
-
-# def fib(n):
-#     if n==0:
-#         return 0
-#     if n==1:
-#         return 1
-#     return fib(n-1) + fib(n-2)
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    return fib(n-1) + fib(n-2)
 
 
 aa = fib(3)
